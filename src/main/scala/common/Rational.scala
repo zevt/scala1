c8463cb616bb28cfffc8bd6887aeea343ae4070d
Viet <vietsemantics@gmail.com>
2019-01-03 03:32:16 -0600

    _

--- src/main/scala/common/Rational.scala
+++ src/main/scala/common/Rational.scala
@@ -4,7 +4,7 @@
   * www.zeroexception.com
   */
 
-class Rational(n: Int, d: Int) {
+class Rational(n: Int, d: Int = 1) {
   require(d != 0)
   private val g = gcd(n.abs, d.abs)
   val numer: Int = n / g
@@ -14,9 +14,17 @@ class Rational(n: Int, d: Int) {
     if (b == 0) a else gcd(b, a % b)
   }
 
-  override def toString = numer + "/" + denom
+  override def toString = if (denom == 1) numer.toString else numer + "/" + denom
 
   def add(that: Rational): Rational = {
     new Rational(numer * that.denom + that.numer * denom, denom * that.denom)
+  }
+
+  def + (that: Rational): Rational = {
+    add(that)
+  }
+
+  def * (that: Rational): Rational = {
+    new Rational(numer * that.numer, denom * that.denom)
   }
 }
