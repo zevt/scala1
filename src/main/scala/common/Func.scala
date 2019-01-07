e92a8250beb5a9ca559c8d70b4c5bc36dda7dd9b
Viet Quoc Tran <viet.tran@siemens.com>
2019-01-07 16:57:52 -0600

    Func

--- src/main/scala/common/Func.scala
+++ src/main/scala/common/Func.scala
@@ -1,6 +1,11 @@
 package common
 
 object Func {
+  /**
+    * This function demo match syntax in Scala, and wildcard matching using underscore character _
+    * @param x
+    * @return
+    */
   def f(x: Int): String = {
     x match {
       case 0 => "Zero"
