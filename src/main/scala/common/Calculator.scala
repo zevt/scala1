3a24cd3e4144c0383892ea07a462b1113af6c627
Viet Quoc Tran <viet.tran@siemens.com>
2019-01-09 21:50:50 -0600

    add Calculator

---
+++ src/main/scala/common/Calculator.scala
@@ -0,0 +1,13 @@
+package common
+
+/**
+  * (1+2)*(3-4) represented as  1 2 + 3 4 - *
+  */
+object Calculator {
+  def main(args: Array[String]): Unit = {
+    println(args)
+  }
+  def handleNumber(token: String, stack: List[Int]): Boolean = {
+    true
+  }
+}
