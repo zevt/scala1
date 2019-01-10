465e0829bd67b208c8fb393c2b5e2af31edff55c
Viet <vietsemantics@gmail.com>
2019-01-10 00:21:46 -0600

    postfix calculation

--- src/main/scala/common/Calculator.scala
+++ src/main/scala/common/Calculator.scala
@@ -1,12 +1,52 @@
 package common
 
+import scala.collection.mutable.MutableList
+import scala.collection.mutable.Stack
+
 /**
   * (1+2)*(3-4) represented as  1 2 + 3 4 - *
   */
 object Calculator {
   def main(args: Array[String]): Unit = {
-    println(args)
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Require one argument")
+    } else {
+      val expression = args(0)
+      val tokens = expression.split(" ")
+            
+      val stack = new Stack[Int]
+      for (token <- tokens) {
+        token match {
+          case "+" =>
+            val rhs = stack.pop()
+            val lhs = stack.pop()
+            stack.push(rhs + lhs)
+          case "-" =>
+            val rhs = stack.pop()
+            val lhs = stack.pop()
+            stack.push(lhs - rhs)
+          case "*" =>
+            val rhs = stack.pop()
+            val lhs = stack.pop()
+            stack.push(rhs * lhs)
+          case "/" =>
+            val rhs = stack.pop()
+            val lhs = stack.pop()
+            stack.push(lhs / rhs)
+          case _ => try {
+            stack.push(token.toInt)
+          } catch {
+            case _: NumberFormatException => throw new IllegalArgumentException("Invalid Token: " + token)
+          }
+        }
+      }
+      println(stack.head)
+
+    }
+
+
   }
+
   def handleNumber(token: String, stack: List[Int]): Boolean = {
     true
   }
