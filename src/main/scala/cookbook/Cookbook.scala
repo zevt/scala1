bfd29569f42c311000652debc8db0af92db03b2c
Viet Quoc Tran <viet.tran@siemens.com>
2019-01-10 10:29:01 -0600

    apply unapply case class

---
+++ src/main/scala/cookbook/Cookbook.scala
@@ -0,0 +1,4 @@
+package cookbook
+class Cookbook {
+  val recipes: List[Recipe] = List(Recipe(List("pepper"), List("burn pepper")))
+}
---
+++ src/main/scala/cookbook/Recipe.scala
@@ -0,0 +1,18 @@
+package cookbook
+//class Recipe(
+//              val ingredients: List[String] = List.empty,
+//              val directions: List[String] = List.empty
+//            ) {
+//
+//}
+//
+//object Recipe {
+//  def apply(ingredients: List[String], directions: List[String]): Recipe
+//  = new Recipe(ingredients, directions)
+//
+//  def unapply(recipe: Recipe): Option[(List[String], List[String] )] =
+//    Some(recipe.ingredients, recipe.directions)
+//}
+
+case class Recipe(ingredients: List[String] = List.empty,
+                  directions: List[String] = List.empty)
