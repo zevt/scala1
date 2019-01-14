a2db2b3dd218737c709b975b76c2ae542cdd259c
Viet <vietsemantics@gmail.com>
2019-01-13 18:23:38 -0600

    Ordered trait

--- src/main/scala/cookbook/Mass.scala
+++ src/main/scala/cookbook/Mass.scala
@@ -20,3 +20,25 @@ case class Kilogram(amount: Double) extends Mass {
   override def toGrams: Gram = Gram(amount * 1000)
   override def toString: String = amount + "(kg)"
 }
+
+//--------------------------------------------------------------------
+sealed abstract class Mass2 extends Ordered[Mass2] {
+  def amount: Double
+  def toGrams: Gram2
+  override def compare(that: Mass2): Int = (this.toGrams.amount - that.toGrams.amount).toInt
+}
+
+case class Gram2(amount: Double) extends Mass2 {
+  override def toGrams: Gram2 = this
+  override def toString: String = amount + "(g)"
+}
+
+case class Miligram2(amount: Double) extends Mass2 {
+  override def toGrams: Gram2 = Gram2(amount  / 1000)
+  override def toString: String = amount + "(mg)"
+}
+
+case class  Kilogram2(amount: Double) extends Mass2 {
+  override def toGrams: Gram2 = Gram2(amount * 1000)
+  override def toString: String = amount + "(kg)"
+}
