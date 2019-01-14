357d2bcefc0db5ac1954b4c65814c385b24ea3f2
Viet <vietsemantics@gmail.com>
2019-01-13 18:42:53 -0600

    Ordered trait

--- src/main/scala/cookbook/Mass.scala
+++ src/main/scala/cookbook/Mass.scala
@@ -22,23 +22,33 @@ case class Kilogram(amount: Double) extends Mass {
 }
 
 //--------------------------------------------------------------------
-sealed abstract class Mass2 extends Ordered[Mass2] {
-  def amount: Double
+/**
+  * Using Ordered trait
+  * https://www.scala-lang.org/api/2.12.0/scala/math/Ordered.html
+  */
+
+trait Measured {
+  val amount: Double
+  def symbol: String
+  override def toString: String = amount + s"($symbol)"
+}
+
+sealed abstract class Mass2 extends Ordered[Mass2] with Measured {
   def toGrams: Gram2
   override def compare(that: Mass2): Int = (this.toGrams.amount - that.toGrams.amount).toInt
 }
 
 case class Gram2(amount: Double) extends Mass2 {
+  val symbol = "g"
   override def toGrams: Gram2 = this
-  override def toString: String = amount + "(g)"
 }
 
 case class Miligram2(amount: Double) extends Mass2 {
+  val symbol = "mg"
   override def toGrams: Gram2 = Gram2(amount  / 1000)
-  override def toString: String = amount + "(mg)"
 }
 
 case class  Kilogram2(amount: Double) extends Mass2 {
+  val symbol = "kg"
   override def toGrams: Gram2 = Gram2(amount * 1000)
-  override def toString: String = amount + "(kg)"
 }
